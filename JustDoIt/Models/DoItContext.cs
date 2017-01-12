4801d8d817eb7a9734348617af21d23c1e425e53
RayJohnUa <mr.john.ray96@gmail.com>
2017-01-12 13:13:41 +0200

    Mapping Model

--- JustDoIt/Models/DoItContext.cs
+++ JustDoIt/Models/DoItContext.cs
@@ -12,6 +12,9 @@ namespace JustDoIt.Models
             : base("DoItContext")
         {
             Database.SetInitializer<DoItContext>(new DropCreateDatabaseIfModelChanges<DoItContext>());
+            UserModels User = new UserModels() { Mail = "mail@mail.com" , Name ="Romesta" , Password ="password" };
+            this.Users.Add(User);
+            this.SaveChanges();
         }
 
         public virtual DbSet<UserModels> Users { get; set; }
--- JustDoIt/Models/UserModels.cs
+++ JustDoIt/Models/UserModels.cs
@@ -7,18 +7,22 @@ using System.Web;
 
 namespace JustDoIt.Models
 {
-    public class ColumnModels
+    public class UserModels
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
+        public string Mail { get; set; }
+        [Required]
         public string Name { get; set; }
-            
-        public int TaskListId { get; set; }
-        [ForeignKey("TaskListId")]
-        public virtual TaskListModels TaskList { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [NotMapped]
+        public string Token { get; set; }
 
         public virtual ICollection<TaskModels> Tasks { get; set; }
+        public virtual ICollection<TaskListModels> TaskList { get; set; }
     }
 }
